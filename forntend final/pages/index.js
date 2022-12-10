39d656e1ee9bbd4571b3d1d6275de764f9094ccf
imdeveloper100 <imdeveloper100@gmail.com>
2022-12-10 15:10:08 +0500

    Travel book with backend

---
+++ forntend final/pages/index.js
@@ -0,0 +1,14 @@
+import HomePage from "../components/home";
+import Main from "../components/posts";
+import Navbar from "../components/navbar";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <HomePage />
+      <Main />
+    </div>
+  );
+}
